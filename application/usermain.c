a5f11bc82998888e73781316eaf30926fda3bd87
Reimanbow <reimanbow0318@gmail.com>
2026-03-13 16:02:58 +0900

    fix: マルチコアサンプル

--- application/usermain.c
+++ application/usermain.c
@@ -2,153 +2,100 @@
  * @file usermain.c
  */
 #include <trykernel.h>
+#include <knldef.h>
 
-// イベントフラグ生成情報
-ID  flgid;
-T_CFLG  cflg = {
-    .flgatr     = TA_TFIFO | TA_WMUL,   // イベントフラグ属性
-    .iflgptn    = 0,                    // イベントフラグ初期値
-};
+extern void (* const vector_tbl[])();
 
-// セマフォ生成情報
-ID  semid;
-T_CSEM  csem = {
-    .sematr     = TA_TFIFO | TA_FIRST,  // セマフォ属性
-    .isemcnt    = 1,                    // セマフォ資源数の初期値
-    .maxsem     = 1,                    // セマフォ資源数の最大値
-};
+#define FIFO_ST                 (0xD0000050)
+#define FIFO_ST_RDY             (1<<1)
+#define FIFO_ST_VLD             (1<<0)
 
-UW  tskstk_btn[1024/sizeof(UW)];  // ボタン検出タスクのスタック
-ID  tskid_btn;                    // ボタン検出タスクのID番号
-
-void task_btn(INT stacd, void *exinf);
-T_CTSK ctsk_btn = {
-    .tskatr     = TA_HLNG | TA_RNG3 | TA_USERBUF,   // タスク属性
-    .task       = task_btn,
-    .itskpri    = 10,
-    .stksz      = 1024,
-    .bufptr     = tskstk_btn,
-};
-
-UW  tskstk_led1[1024/sizeof(UW)];  // LED制御タスク1のスタック
-ID  tskid_led1;                    // LED制御タスク1のID番号
-
-void task_led1(INT stacd, void *exinf);
-T_CTSK ctsk_led1 = {
-    .tskatr     = TA_HLNG | TA_RNG3 | TA_USERBUF,   // タスク属性
-    .task       = task_led1,
-    .itskpri    = 10,
-    .stksz      = 1024,
-    .bufptr     = tskstk_led1,
-};
-
-UW  tskstk_led2[1024/sizeof(UW)];  // LED制御タスク2のスタック
-ID  tskid_led2;                    // LED制御タスク2のID番号
-
-void task_led2(INT stacd, void *exinf);
-T_CTSK ctsk_led2 = {
-    .tskatr     = TA_HLNG | TA_RNG3 | TA_USERBUF,   // タスク属性
-    .task       = task_led2,
-    .itskpri    = 10,
-    .stksz      = 1024,
-    .bufptr     = tskstk_led2,
-};
+#define FIFO_WR                 (SIO_BASE+0x54)
+#define FIFO_RD                 (SIO_BASE+0x58)
 
 /**
- * @brief ボタン検出タスクの実行関数
+ * @brief コア1起床関数
+ * @param UW *vtbl  例外ベクタテーブルのアドレス
+ * @param UW *sp    スタックポインタの値
+ * @param FP ent    実行開始アドレス
  */
-void task_btn(INT stacd, void *exinf) {
-    UW      btn, btn0, diff;
+void mcore_wup_core1(UW *vtbl, UW *sp, FP ent) {
+    UW      cmd[] = {0, 0, 1, (UW)vtbl, (UW)sp, (UW)ent};
+    UW      res;
+    UINT    seq = 0;
 
-    // GP13をスイッチ入力に設定
-    out_w(GPIO(13), (in_w(GPIO(13)) | GPIO_PUE) & ~GPIO_PDE);   // PULLUP設定
-    out_w(GPIO_OE_CLR, (1<<13));                                // 出力無効
-    out_w(GPIO_CTRL(13), 5);
+    do {
+        if (!cmd[seq]) {
+            while (in_w(FIFO_ST) & FIFO_ST_VLD) in_w(FIFO_RD);  // FIFOを空にする
+            __asm__ volatile("sev");    // SEV命令発行
+        }
 
-    // GP14をスイッチ入力に設定
-    out_w(GPIO(14), (in_w(GPIO(14)) | GPIO_PUE) & ~GPIO_PDE);   // PULLUP設定
-    out_w(GPIO_OE_CLR, (1<<14));                                // 出力無効
-    out_w(GPIO_CTRL(14), 5);
-    
-    btn0 = in_w(GPIO_IN) & ((1<<14)|(1<<13));
+        while ((in_w(FIFO_ST) & FIFO_ST_RDY) == 0);     // FIFOが送信可能になるまで待つ
+        out_w(FIFO_WR, cmd[seq]);                       // FIFOにデータを書き込む
+        __asm__ volatile("sev");                        // SEV命令発行
+
+        while ((in_w(FIFO_ST) & FIFO_ST_VLD) == 0) {    // FIFOのデータを待つ
+            __asm__ volatile("wfe");                    // WFE命令発行
+        }
+        res = in_w(FIFO_RD);
+
+        if (cmd[seq] == res) {
+            seq++;      // Core1からの応答の確認
+        } else {
+            seq = 0;    // エラー 最初からやり直し
+        }
+    } while (seq < sizeof(cmd) / sizeof(UW));
+}
+
+void func_core1(void) {
+    UINT    cnt;
+
+    // LEDのポートの初期化
+    // GP15端子出力無効
+    out_w(GPIO_OE_CLR, (1<<15));
+    // GP15端子出力をクリア
+    out_w(GPIO_OUT_CLR, (1<<15));
+    // GP15端子 SIO
+    out_w(GPIO_CTRL(15), 5);
+    // GP15出力有効
+    out_w(GPIO_OE_SET, (1<<15));
+
+    // タイマの初期化
+    out_w(SYST_CSR, SYST_CSR_CLKSOURCE);
+    out_w(SYST_RVR, (TIMER_PERIOD*TMCLK_KHz)-1);
+    out_w(SYST_CVR, (TIMER_PERIOD*TMCLK_KHz)-1);
+    out_w(SYST_CSR, SYST_CSR_CLKSOURCE | SYST_CSR_ENABLE);
+
+    // LEDの点滅
     while (1) {
-        btn = in_w(GPIO_IN) & ((1<<14)|(1<<13));  // スイッチ読み込み
-        if ((diff = btn ^ btn0) != 0) {
-            if ((diff & (1<<13)) && !(btn & (1<<13))) {
-                tm_putstring("BTN-1 ON\n");
-                tk_set_flg(flgid, (1<<1));      // イベントフラグのセット
+        out_w(GPIO_OUT_XOR, 1<<15);
+
+        cnt = 500 / TIMER_PERIOD;
+        while (cnt) {
+            if ((in_w(SYST_CSR) & SYST_CSR_COUNTFLAG) != 0) {
+                cnt--;
             }
-            if ((diff & (1<<14)) && !(btn & (1<<14))) {
-                tm_putstring("BTN-0 ON\n");
-                tk_set_flg(flgid, (1<<0));      // イベントフラグのセット
-            }
-            btn0 = btn;
         }
-        tk_dly_tsk(100);    // 0.1秒待ち
     }
 }
 
-/**
- * @brief LED制御1タスクの実行関数
- */
-void task_led1(INT stacd, void *exinf) {
-    UINT flgptn;
-
-    while (1) {
-        tk_wai_flg(flgid, (1<<0), TWF_ANDW | TWF_BITCLR, &flgptn, TMO_FEVR);    // フラグ待ち
-
-        tk_wai_sem(semid, 1, TMO_FEVR);     // 資源の獲得
-        for (INT i = 0; i < 3; i++) {
-            out_w(GPIO_OUT_SET, (1<<25));   // LEDの点灯
-            tk_dly_tsk(500);                // 0.5秒待ち
-            out_w(GPIO_OUT_CLR, (1<<25));   // LEDの消灯
-            tk_dly_tsk(500);                // 0.5秒待ち
-        }
-        tk_sig_sem(semid, 1);               // 資源の返却
-    }
-}
-
-/**
- * @brief LED制御2タスクの実行関数
- */
-void task_led2(INT stacd, void *exinf) {
-    UINT flgptn;
-
-    while (1) {
-        tk_wai_flg(flgid, (1<<1), TWF_ANDW | TWF_BITCLR, &flgptn, TMO_FEVR);    // フラグ待ち
-
-        tk_wai_sem(semid, 1, TMO_FEVR);     // 資源の獲得
-        for (INT i = 0; i < 5; i++) {
-            out_w(GPIO_OUT_SET, (1<<25));   // LEDの点灯
-            tk_dly_tsk(100);                // 0.1秒待ち
-            out_w(GPIO_OUT_CLR, (1<<25));   // LEDの消灯
-            tk_dly_tsk(100);                // 0.1秒待ち
-        }
-        tk_sig_sem(semid, 1);               // 資源の返却
-    }
-}
-
-/**
- * @brief メイン関数
- */
 int usermain(void) {
-    // イベントフラグの生成
-    flgid = tk_cre_flg(&cflg);
+    // LEDのポートの初期化
+    // GP14端子出力無効
+    out_w(GPIO_OE_CLR, (1<<14));
+    // GP14端子出力をクリア
+    out_w(GPIO_OUT_CLR, (1<<14));
+    // GP14端子 SIO
+    out_w(GPIO_CTRL(14), 5);
+    // GP14出力有効
+    out_w(GPIO_OE_SET, (1<<14));
 
-    // セマフォの生成
-    semid = tk_cre_sem(&csem);
+    // CORE1を起床
+    mcore_wup_core1((UW*)vector_tbl, (UW*)0x20042000, (FP)func_core1);
 
-    // ボタン検出タスクの生成、実行
-    tskid_btn = tk_cre_tsk(&ctsk_btn);
-    tk_sta_tsk(tskid_btn, 0);
-
-    // LED制御タスク1の生成、実行
-    tskid_led1 = tk_cre_tsk(&ctsk_led1);
-    tk_sta_tsk(tskid_led1, 0);
-
-    // LED制御タスク2の生成、実行
-    tskid_led2 = tk_cre_tsk(&ctsk_led2);
-    tk_sta_tsk(tskid_led2, 0);
-
-    return 0;
+    // LEDの点灯
+    while (1) {
+        out_w(GPIO_OUT_XOR, 1<<14);
+        tk_dly_tsk(500);
+    }
 }
